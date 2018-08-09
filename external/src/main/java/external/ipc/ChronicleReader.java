fd97352a970a2b4302f7b88abcea0f7380ee2bac
Isaac Tyan <isaac.tyan@gmail.com>
2018-08-08 23:23:06 -0400

    enhanced Message framework added MessagingFactory to encapsulate messaging inter-mutability

--- external/src/main/java/external/ipc/ChronicleReader.java
+++ external/src/main/java/external/ipc/ChronicleReader.java
@@ -2,8 +2,8 @@ package external.ipc;
 
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
-import common.core.message.MessageFactory;
-import common.core.message.MutableMessage;
+import common.core.message.Message;
+import common.core.message.MessagingFactory;
 import net.openhft.chronicle.bytes.Bytes;
 import net.openhft.chronicle.queue.ExcerptTailer;
 
@@ -13,11 +13,14 @@ final class ChronicleReader implements WireReader {
 
     private final ExcerptTailer tailer;
     private final ByteBuffer buffer;
-    private final MutableMessage message = null;
+    private final MessagingFactory messagingFactory;
     private final Bytes bytes;
 
     @Inject
-    ChronicleReader(@Assisted String path) {
+    ChronicleReader(MessagingFactory messagingFactory,
+                    @Assisted String path) {
+        this.messagingFactory = messagingFactory;
+
         tailer = ChronicleFactory.createTailer(path);
         buffer = ByteBuffer.allocate(512);
         bytes = Bytes.wrapForWrite(buffer);
@@ -26,7 +29,7 @@ final class ChronicleReader implements WireReader {
     @Override
     public void poll(WireDataHandler handler) {
         if (tailer.readBytes(bytes)) {
-            MessageFactory.formulate(buffer, message);
+            Message message = messagingFactory.compose(buffer);
             handler.handle(message);
         }
     }
